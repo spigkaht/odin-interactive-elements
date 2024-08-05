99c2cf3d5f6361e6b5f69b3ef79da13b72b08aec
Benjamin Jackson <benj@betterrentals.com.au>
2024-08-05 20:14:47 +1000

    completed exercise

--- carousel/script.js
+++ carousel/script.js
@@ -1,14 +1,24 @@
 const leftNav = document.querySelector("#left");
 const rightNav = document.querySelector("#right");
 const images = document.querySelectorAll(".carousel-image");
+const navdots = document.querySelectorAll(".navdot")
+const currentImage = document.querySelector(".active");
+navdots[currentImage.id - 1].src = "./images/dot-filled.svg"
 
 leftNav.addEventListener("click", (e) => {
   const currentImage = document.querySelector(".active");
-
+  prevImage = currentImage.id == 1 ? images[4] : images[currentImage.id - 2]
+  currentImage.classList.toggle("active");
+  prevImage.classList.toggle("active");
+  navdots[currentImage.id].src = "./images/dot-hollow.svg"
+  navdots[currentImage.id - 1].src = "./images/dot-filled.svg"
 });
 
 rightNav.addEventListener("click", (e) => {
   const currentImage = document.querySelector(".active");
+  nextImage = currentImage.id == 5 ? images[0] : images[currentImage.id]
   currentImage.classList.toggle("active");
-  currentImage.nextSibling.classList.toggle("active");
+  nextImage.classList.toggle("active");
+  navdots[currentImage.id].src = "./images/dot-hollow.svg"
+  navdots[currentImage.id - 1].src = "./images/dot-filled.svg"
 });
